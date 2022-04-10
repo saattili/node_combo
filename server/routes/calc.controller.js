6f5a186d417895ae05ee18a7dffc91ebbfc35e27
saattili <attsmadhav@gmail.com>
2022-04-10 16:36:59 +0530

    DockerFile added

--- server/routes/calc.controller.js
+++ server/routes/calc.controller.js
@@ -1,21 +1,83 @@
-const path = require("path")
+const path = require("path");
+const {spawn } = require("child_process");
+
+
 
 
 function calculate(req,res){
-    var myData = req.body.mydata;
-    result = getallCombos(myData);
-    console.log(req.body.mydata)
-    //do cal here
-    //console.log(path.join(__dirname,'..','..'));
-    res.render('index',{
-        title: "Combinations",
-        result: result,
-    });
+
+    var combos = [];
+    var data = req.body.mydata
+    var verified = [];
+    arr = data.split('');
+   
+    arr = arr.sort();
+    min = arr.toString();
+    min = min.replaceAll(',','')
+    min = Number(min)
+    max = arr.reverse().toString();
+    max = max.replaceAll(',','');
+    max = Number(max)
+for(min;min<=max;min++){
+    numStr = min.toString();
+    numArr = numStr.split('');
+    
+    var flag = 0;
+    for(let val = 0;val<arr.length;val++){
+        console.log(`checking ${arr[val]}   with ${numArr}`)
+        if(!(arr[val] in numArr)){
+            flag = 1;
+            break;
+        }
+            
+    }
+    if(flag === 0){
+        combos.push(min);
+    }
+
+
+  
 }
 
-function getallCombos(myData){
+    res.render('index',{
+        title: "Combinations",
+         result: combos,
+        
+
+});
+
+
+// let pyRes = new Promise((resolve,reject) =>{
+
+//     var myData = req.body.mydata;
+//     const pythonProcess = spawn('python3',[path.join(__dirname,'..','..','py_utils','get_combos.py'), myData]);
+//     pythonProcess.stdout.on('data',(data)=>{
+//         resolve(data);
+//     });
+
+//     pythonProcess.stderr.on('data',(data)=>{
+//         reject(data);
+//     });
+
+// });
+
+
+//     pyRes.then((pyData) => {
+
+//         console.log("Pydata came from promise..................")
+
+//         res.render('index',{
+//             title: "Combinations",
+//             result: pyData,
+//         });
+
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     });
     
 }
+
     //res.sendFile(path.join(__dirname,'..','..','views','index.hbs'));
 
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const PORT = 8080;
+const HOST = '0.0.0.0';
 const http = require('http');
 const app = require('./app');
 
 server = http.createServer(app);
 
-server.listen(PORT,()=>{
-console.log(`server running on ${PORT}...`);
+server.listen(PORT,HOST,()=>{
+console.log(`server running on ${PORT}... on HOST ${HOST}`);
 });
 
